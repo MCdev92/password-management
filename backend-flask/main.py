505b3f660e4d9bc0588e38c0d4d18aac4b12a5ee
Manuel Corporan <110692284+MCdev92@users.noreply.github.com>
2023-10-24 08:36:55 -0700

    password app coded waiting for security features!

--- backend-flask/main.py
+++ backend-flask/main.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify
 from flask_restful import Api, Resource
-from sqlalchemy.orm import Session
 from database import SessionLocal
 from models import Passwords
 
@@ -9,95 +8,80 @@ api = Api(app)
 
 class PasswordRes(Resource):
     def get(self, password_id):
-         # Retrieve a password by its ID
-        db = SessionLocal()
-        password = db.query(Passwords).filter(Passwords.id == password_id).first()
-        db.close()
-        
-        if password:
-            # Convert the password object to a dictionary
-            password_data = {
-                'id': password.id,
-                'username': password.username,
-                'website': password.website,
-                'password': password.password,
-                'note': password.note
-            }
-            return jsonify(password_data)
-        else:
-            return jsonify({'message': 'Password not found'}), 404
+        # Retrieve a password by its ID
+        with SessionLocal() as db:
+            password = db.query(Passwords).filter(Passwords.id == password_id).first()
+            if password:
+                password_data = {
+                    'id': password.id,
+                    'username': password.username,
+                    'website': password.website,
+                    'password': password.password,
+                    'note': password.note
+                }
+                return jsonify(password_data)
+            else:
+                return jsonify({'message': 'Password not found'}), 404
 
     def post(self):
-         # 1. Request Data Handling
+        # Request Data Handling
         data = request.get_json()
 
         if not data:
             return jsonify({"message": "No data provided in the request"}), 400
 
-        # 2. Data Validation
+        # Data Validation
         required_fields = ["title", "username", "password", "website"]
         for field in required_fields:
             if field not in data:
                 return jsonify({"message": f"'{field}' is a required field"}), 400
 
-        # 3. Database Interaction
-        db = SessionLocal()
-        new_password = Passwords(
-            username=data["username"],
-            title=data["title"],
-            password=data["password"],
-            website=data["website"]
-        )
-        db.add(new_password)
-        db.commit()
-        db.close()
+        # Database Interaction
+        with SessionLocal() as db:
+            new_password = Passwords(**data)
+            db.add(new_password)
+            db.commit()
 
-
-    def put(self):
-        # 1. Request Data Handling
-        data = request.get_json()
-
-        if not data:
-            return jsonify({"message": "No data provided in the request"}), 400
-
-        # 2. Data Validation
-        required_fields = ["title", "username", "password", "website"]
-        for field in required_fields:
-            if field not in data:
-                return jsonify({"message": f"'{field}' is a required field"}), 400
-
-        # 3. Database Interaction
-        db = SessionLocal()
-        new_password = Passwords(
-            username=data["username"],
-            title=data["title"],
-            password=data["password"],
-            website=data["website"]
-        )
-        db.add(new_password)
-        db.commit()
-        db.close()
-
-        # 4. Response
         return jsonify({"message": "Password created successfully"})
 
+    def put(self):
+        # Request Data Handling
+        data = request.get_json()
+
+        if not data:
+            return jsonify({"message": "No data provided in the request"}), 400
+
+        # Data Validation
+        required_fields = ["title", "username", "password", "website"]
+        for field in required_fields:
+            if field not in data:
+                return jsonify({"message": f"'{field}' is a required field"}), 400
+
+        # Database Interaction
+        with SessionLocal() as db:
+            password_id = data.get('id')
+            password = db.query(Passwords).filter(Passwords.id == password_id).first()
+            if not password:
+                return jsonify({"message": "Password not found"}, 404)
+            # Update the password entry
+            for field in data:
+                setattr(password, field, data[field])
+            db.commit()
+
+        return jsonify({"message": "Password updated successfully"})
+
     def delete(self, password_id):
-        # 1. URL Parameter Handling
+        # Database Interaction
+        with SessionLocal() as db:
+            password = db.query(Passwords).filter(Passwords.id == password_id).first()
 
-        # 2. Database Interaction
-        db = SessionLocal()
-        password = db.query(Passwords).filter(Passwords.id == password_id).first()
+            if not password:
+                return jsonify({"message": "Password not found"}, 404)
 
-        if not password:
-            db.close()
-            return jsonify({"message": "Password not found"}, 404)
+            # Delete the password entry
+            db.delete(password)
+            db.commit()
 
-        # Delete the password entry
-        db.delete(password)
-        db.commit()
-        db.close()
-
-        # 3. Response
         return jsonify({"message": "Password deleted successfully"})
 
 
